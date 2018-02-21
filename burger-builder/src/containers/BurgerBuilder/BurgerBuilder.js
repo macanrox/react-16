2071aec41651609866847a52625566e99151a79a
Bresia Prudente <macanrox@gmail.com>
2018-02-20 21:33:38 -0600

    Added order button and began early implementation of an order summary button. For some reason, the modal toggle doesn’t seem to work for me.

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+//default global prices for the ingredients
+const INGREDIENT_PRICES = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
 	// constructor (props) {
@@ -15,14 +26,85 @@ class BurgerBuilder extends Component {
 			bacon: 0,
 			cheese: 0,
 			meat: 0
-		}
+		},
+		totalPrice: 4,
+		purchasable: false,
+		purchasing: false
 	}//end state
 
+	//	Checks the total number of ingredients in the burger
+	//	and determine if there's enough to order
+	updatePurchaseState (ingredients) {
+		const sum = Object.keys(ingredients)
+			.map(igKey => {
+				return ingredients[igKey];
+			})
+			.reduce((sum, el) => {
+				return sum + el;
+			}, 0);
+		this.setState({purchasable: sum > 0});
+	}//end updatePurchaseState
+
+	addIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount + 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceAddition = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + priceAddition;
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+		this.updatePurchaseState(updatedIngredients);
+	}//end addIngredientHandler
+
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		//	can't have less than 0 ingredient count
+		if (oldCount <= 0) {
+			return;
+		}
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceDeduction;
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+		this.updatePurchaseState(updatedIngredients);
+	}//end removeIngredientHandler
+
+	purhaseHandler = () => {
+		this.setState({purchasing: true});
+	}//end purchaseHandler
+
 	render () {
+		const disabledInfo = {
+			...this.state.ingredients
+		};
+		for (let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0
+		}
+		//	remember that the state at this point looks like
+		//	{salad: true, bacon: false}
+		//		* we want to turn the true state to false so that
+		//		* the user can't remove less than 0 ingredients
 		return (
 			<Aux>
+				<Modal show={this.state.purchasing}>
+					<OrderSummary ingredients={this.state.ingredients} />
+				</Modal>
 				<Burger ingredients={this.state.ingredients} />
-				<div>Build Controls</div>
+				<BuildControls 
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+					purchasable={this.state.purchasable}
+					ordered={this.purchaseHandler}
+					price={this.state.totalPrice} />
 			</Aux>
 		);
 	}//end render ()
