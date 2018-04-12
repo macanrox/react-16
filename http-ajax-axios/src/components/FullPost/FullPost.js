2bae5bb8e38aad05e87db9b927a6e9de36eefaae
macanrox <macanrox@gmail.com>
2018-04-12 12:19:01 -0500

    Added .gitignore and updated folder to finished learning module

--- http-ajax-axios/src/components/FullPost/FullPost.js
+++ http-ajax-axios/src/components/FullPost/FullPost.js
@@ -13,12 +13,18 @@ class FullPost extends Component {
             if ( !this.state.loadedPosts || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
 
             }
-            axios.get('http://jsonplaceholder.typicode.com/posts' + this.props.id).then(response => {
+            axios.get('/posts' + this.props.id).then(response => {
                     //console.log(response);
                     this.setState({loadedPost: response.data});
             });
         }
     }// end componentDidUpdate()
+
+    deletePostHandler = () => {
+        axios.delete('/posts' + this.props.id).then(response => {
+            console.log(response);
+        });
+    }
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
@@ -31,7 +37,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
             );
